6e17286145e9ed800cbae7922667cd8779c5d1e2
Amrit Singhal <amritsinghal97@gmail.com>
2018-02-11 16:29:18 +0530

    added a model file

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ make IMAGEPATH=/data/amrits/ugp/images/
 
 cd ../semantic_features
 
-python test.py --test_img_file /data/amrits/ugp/image_files.txt --test_img_directory /data/amrits/ugp/images/ --weights_encoder /data/amrits/ugp/weights/encoder_best.pth --weights_decoder /data/amrits/ugp/weights/decoder_best.pth 
+python test.py --test_img_file /data/amrits/ugp/valence.txt --test_img_directory /data/amrits/ugp/images/ --weights_encoder /data/amrits/ugp/weights/encoder_best.pth --weights_decoder /data/amrits/ugp/weights/decoder_best.pth 
 
 cd ..
 
-python train.py /data/amrits/ugp/image_files.txt /data/amrits/ugp/images/
+python train.py /data/amrits/ugp/valence.txt /data/amrits/ugp/images/
 
